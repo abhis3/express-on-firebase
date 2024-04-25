99aa34a65739e586f0d597794d0882c46d9c658f
Tony Huang <tony.huang.jun@gmail.com>
2024-04-25 13:06:50 -0400

    api key

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express 2", env: process.env.TEST_ENV });
+  res.render("index", { title: "Express 2", env: process.env.FIREBASE_API_KEY });
 });
 
 module.exports = router;
