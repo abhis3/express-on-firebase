08520f166ce3357b017f7473f21eb33195d52e13
Abhinav Sundaresan <abhisun@google.com>
2024-06-12 11:20:45 -0400

    initial boilerplate for NODE_ENV testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
+// Log all environment variables
+console.log('Environment Variables:', process.env); // Print entire object
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express 2", env: process.env.FIREBASE_API_KEY });
+  res.render("index", { title: "Express 2", env: process.env.FIREBASE_API_KEY, node: process.env.NODE_ENV });
 });
 
 module.exports = router;
